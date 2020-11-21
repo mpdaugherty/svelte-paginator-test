f7118492fd6f64a7716079c3a86eed62f31a270f
Michael Daugherty <mike@mpdaugherty.com>
2020-11-21 18:36:30 +0800

    Add a test of overriding classes

--- src/App.svelte
+++ src/App.svelte
@@ -12,10 +12,36 @@
  }
 </script>
 
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+
 <main>
   <h1>Svelte-Paginator!</h1>
 
-  <Paginator endpoint={loadLetters} let:items perPage={4} numPageLinks={11}>
+  <Paginator endpoint={loadLetters} let:items perPage={4} numPageLinks={12}>
+    {#if items === null}
+      Loading...
+    {:else}
+      <div>
+        {#each items as letter}
+          {letter}&nbsp;
+        {/each}
+      </div>
+    {/if}
+  </Paginator>
+
+  <br/>
+
+  <h1>
+    Svelte-Paginator!
+    <span>&laquo;With Bootstrap Class Overrides&raquo;</span>
+  </h1>
+
+  <Paginator endpoint={loadLetters}
+                      let:items perPage={4}
+                      numPageLinks={12}
+             class_button="btn btn-outline-secondary"
+             class_current_page="btn btn-secondary"
+             class_button_group="btn-group">
     {#if items === null}
       Loading...
     {:else}
@@ -41,6 +67,14 @@
    text-transform: uppercase;
    font-size: 4em;
    font-weight: 100;
+   margin-bottom: 20px;
+ }
+
+ h1>span {
+   display: block;
+   font-size: 0.333em;
+   font-style: italic;
+   margin-top: -0.167em
  }
 
  @media (min-width: 640px) {
